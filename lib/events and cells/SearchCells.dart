ed64864eef544aaad46cefdaff8bf1d5e875df4f
Mayank-1200 <mayankpansuriya86@gmail.com>
2023-09-30 07:54:32 +0530

    Search function in cells module and added logout button for event and cell admin.

---
+++ lib/events and cells/SearchCells.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../minor screens/searchPage.dart';
+import 'Search.dart';
+class SearchBar extends StatelessWidget {
+  SearchBar({super.key});
+  int color=0xFF98e8e8;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Search()));
+      },
+      child: Container(
+        height: 35,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(color: Color(color))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(children: [Icon(
+                Icons.search,
+                color: Colors.grey.shade600,
+              ),
+                Text(
+                  'What are you looking for?',
+                  style: TextStyle(color: Colors.grey.shade600, fontSize: 18),
+                )],),
+            ),
+            Container(
+              height: 32,
+              width: 75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),color: Color(color),),
+              child: Center(child: Text('Search',style: TextStyle(color: Colors.grey),)),
+            )
+          ],
+        ),
+      ),
+    );;
+  }
+}
